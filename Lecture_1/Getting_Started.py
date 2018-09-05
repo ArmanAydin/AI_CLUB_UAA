3622877bae4e3234a53b1f753e7a77d1b63e469a
ArmanAydin <42741546+ArmanAydin@users.noreply.github.com>
2018-09-05 15:04:15 +0300

    Update Getting_Started.py

--- Lecture_1/Getting_Started.py
+++ Lecture_1/Getting_Started.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
 
+img1 = cv2.imread('MyPic.png', -1)
+img2 = cv2.imread('MyPic.png', 0)
+img3 = cv2.imread('MyPic.png', 1)
+
+cv2.imshow('image1', img1)
+cv2.imshow('image2', img2)
+cv2.imshow('image3', img3)
+cv2.waitKey(0) & 0xFF
+cv2.destroyAllWindows()
