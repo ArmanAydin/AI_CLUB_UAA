3509a94f6119c1c8d1a3ca9d99cc41da5ec100b9
ArmanAydin <42741546+ArmanAydin@users.noreply.github.com>
2018-09-06 13:00:20 +0300

    Create Resizing.py

---
+++ Lecture3/Chapter_3/Resizing.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('MyPic.png')
+
+res = cv2.resize(img,None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+
+cv2.imshow('img', res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#OR
+""" height, width = img.shape[:2]
+    res = cv2.resize(img,(2*width,2*height), interpolation=cv2.INTER_CUBIC)
+"""
